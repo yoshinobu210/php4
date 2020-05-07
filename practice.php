9b160c45c6b3567966e2a873b6a638f297f52433
“yoshinobu210“ <anc58558@ozzio.jp>
2020-05-07 07:05:52 +0000

    PHP/Laravel 02 PHPの型と基本的な式を理解しよう

---
+++ practice.php
@@ -0,0 +1,31 @@
+<?php
+echo 'hello php!';
+$a = 3;
+$b = 7;
+echo $a + $b;
+$array_month = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
+echo $array_month[7];
+$hello = "Hello, ";
+$name = "谷口慶伸";
+$world = "'s world";
+echo $hello . $name . $world;
+$tech_boost = 'tech ';
+$tech_boost .= 'boost';
+echo $tech_boost;
+$calender_2018 = [
+  "January" => "1月",
+  "February" => "2月",
+  "March" => "3月",
+  "April" => "4月",
+  "May" => "5月",
+  "June" => "6月",
+  "July" => "7月",
+  "August" => "8月",
+  "September" => "9月",
+  "October" => "10月",
+  "November" => "11月",
+  "December" => "12月"
+];
+echo $calender_2018["December"];
+
+
